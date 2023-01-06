994e9e0f47bea41271e79a19c4e1fd680d2b50a2
Lucas Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2023-01-06 09:29:30 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const inputTextToConvert = document.getElementById('input-text');
+const inputTextConverted = document.getElementById('input-text-converted');
+const buttonsToExecuteCommand = document.querySelectorAll('.btn-command');
+const buttonClipboard = document.querySelector('.btn-clipboard');
+
+const COMMANDS_TO_EXECUTE = {
+  sentence_case: convertToSenteceCase,
+  lower_case: convertLoweCase,
+  upper_case: convertToUppercase,
+  title_case: convertToTitleCase,
+}
+
+function convertToSenteceCase(text) {
+  const textConverted = String(text)[0].toUpperCase() + String(text).slice(1);
+  setConvertedText(textConverted);
+}
+
+function convertLoweCase(text) {
+  const textConverted = String(text).toLowerCase();
+  setConvertedText(textConverted);
+}
+
+function convertToUppercase(text) {
+  const textConverted = String(text).toUpperCase();
+  setConvertedText(textConverted);
+}
+
+function convertToTitleCase(text) {
+  const textConverted = String(text)
+    .split(' ')
+    .map(word => (word[0]).toUpperCase() + word.slice(1))
+    .join(' ');
+
+  setConvertedText(textConverted)
+}
+
+function setConvertedText(textConverted) {
+  inputTextConverted.value = textConverted;
+}
+
+buttonsToExecuteCommand.forEach((button) => {
+  button.addEventListener('click', () => {
+    const textButton = button.textContent;
+    const command = textButton.split(' ').join('_').toLocaleLowerCase();
+    const textToConvert = inputTextToConvert.value;
+    COMMANDS_TO_EXECUTE[command](textToConvert);
+  })
+});
+
+buttonClipboard.addEventListener('click', async () => {
+  const textToCopy = inputTextConverted.value;
+  navigator.clipboard.writeText(textToCopy);
+});
